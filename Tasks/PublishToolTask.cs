e12602838ad3e8f0c9c0381542ee647894c3a2b8
Christopher Whitley <aristurtledev@gmail.com>
2024-04-28 16:52:31 -0400

    Output log to figure out what's going on

--- Tasks/PublishToolTask.cs
+++ Tasks/PublishToolTask.cs
@@ -25,20 +25,41 @@ public sealed class PublishToolTask : AsyncFrostingTask<BuildContext>
             };
         }
 
-        var copyToDir = $"artifacts-{rid}";
-        if (context.BuildSystem().IsRunningOnGitHubActions)
+        try
         {
-            await context.BuildSystem().GitHubActions.Commands.UploadArtifact(DirectoryPath.FromString(context.ArtifactsDir), copyToDir);
-        }
-        else
-        {
-            // When running locally, make the artifacts directory mimic what github would look like
-            var files = Directory.GetFiles(context.ArtifactsDir);
-            context.CreateDirectory(new DirectoryPath($"{context.ArtifactsDir}/{copyToDir}"));
-            foreach (var file in files)
+            var copyToDir = $"artifacts-{rid}";
+            if (context.BuildSystem().IsRunningOnGitHubActions)
             {
-                context.MoveFileToDirectory(file, new DirectoryPath($"{context.ArtifactsDir}/{copyToDir}"));
+                await context.BuildSystem().GitHubActions.Commands.UploadArtifact(DirectoryPath.FromString(context.ArtifactsDir), copyToDir);
             }
+            else
+            {
+                // When running locally, make the artifacts directory mimic what github would look like
+                var files = Directory.GetFiles(context.ArtifactsDir);
+                context.CreateDirectory(new DirectoryPath($"{context.ArtifactsDir}/{copyToDir}"));
+                foreach (var file in files)
+                {
+                    context.MoveFileToDirectory(file, new DirectoryPath($"{context.ArtifactsDir}/{copyToDir}"));
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            context.Information(" ");
+            context.Information("++++++++++++++++++++++++++++++++++++++++++++++++++");
+            context.Information("Exception");
+            context.Information(ex.Message);
+            if (ex.InnerException is not null)
+            {
+                context.Information(" ");
+                context.Information("Inner Exception");
+                context.Information(ex.InnerException.Message);
+                context.Information(" ");
+            }
+            context.Information(" ");
+            context.Information(ex.StackTrace);
+            context.Information("++++++++++++++++++++++++++++++++++++++++++++++++++");
+            context.Information(" ");
         }
     }
 }
